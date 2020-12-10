48b5ebe63207b11eecd7dd7920ebafca61aeffdf
Felixdavid12 <felixdavidgom@gmail.com>
2020-12-10 08:10:56 -0500

    cambio en línea 21 de UserBalance.vue, cajero-app >>> cajero-api

--- src/components/UserBalance.vue
+++ src/components/UserBalance.vue
@@ -18,7 +18,7 @@ export default {
   created: function(){
     this.username = this.$route.params.username
     let self = this
-    axios.get("https://cajero-app12.herokuapp.com/user/balance/" + this.username)
+    axios.get("https://cajero-api12.herokuapp.com/user/balance/" + this.username)
       .then((result) => {
         self.balance = result.data.balance
       })
